6f9ebdf9f18cf9d3efafa3f7108c24539e919273
Yair <yair.zavaleta@gnp.com.mx>
2019-09-11 16:41:07 -0500

    Proyecto de Flutter

---
+++ cotizador_agente/android/app/src/main/kotlin/mx/com/gnp/cotizador_agente/MainActivity.kt
@@ -0,0 +1,13 @@
+package mx.com.gnp.cotizador_agente
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
