dc0da5fee04bc70ebe3fbdd0dbc1f61e1126b70c
cesarTix <cesar.sumano@tixmedia.com>
2019-09-12 13:27:31 -0500

    first view

--- cotizador_agente/lib/main.dart
+++ cotizador_agente/lib/main.dart
@@ -7,12 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cotizador GMM',
+      title: 'GNP',
       theme: ThemeData(
 
         primarySwatch: Colors.grey,
       ),
-      home: MyHomePage(title: 'Cotizador GMM'),
+      home: MyHomePage(title: 'GNP'),
     );
   }
 }
@@ -26,7 +26,16 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+
+void changeView (){
+  print('hellow');
+}
+
+
+
 class _MyHomePageState extends State<MyHomePage> {
+  final colorHex = const Color(0xFFCECFD1);
+  final colorLetters = const Color(0xFF002E71);
 
   @override
   Widget build(BuildContext context) {
@@ -40,11 +49,133 @@ class _MyHomePageState extends State<MyHomePage> {
 
         child: Column(
 
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+
+            Container(
+              color: colorHex,
+              height: 80,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Icon(Icons.adjust, size: 10.0, color: Colors.transparent,),
+
+                  OutlineButton(
+                    textColor: Colors.orange,
+                    child: Text('COTIZACIONES GUARDADAS'),
+                    onPressed: () {},
+                    borderSide: BorderSide(
+                      color: Colors.orange, //Color of the border
+                      style: BorderStyle.solid, //Style of the border
+                      width: 0.8, //width of the border
+                    ),
+                  ),
+                  Icon(Icons.adjust, size: 25.0, color: Colors.transparent,),
+                  Icon(Icons.adjust, size: 25.0, color: Colors.transparent,),
+
+                Text(
+                  'MÁS',
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontWeight: FontWeight.normal, color: Colors.orange, fontSize: 16),
+                ),
+                Icon(Icons.more_vert, color: Colors.orange,  size: 35.0),
+
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              height: 80,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(Icons.adjust, size: 10.0, color: Colors.transparent,),
+                    Text(
+                      'COTIZACIÓN',
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontWeight: FontWeight.normal, color: colorLetters, fontSize: 19),
+                    ),
+
+                  ],
+
+              ),
+            ),
+            Container(
+              color: colorHex,
+              height: 100,
+              width: double.infinity,
+              child: Row(
+
+
+                children: <Widget>[
+                 Row(
+
+                   children: <Widget>[
+                  Column(
+
+                  children: <Widget>[
+
+                  Text(
+                    'Datos personales',
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontWeight: FontWeight.bold, color: colorLetters, fontSize: 16),
+                  ),
+
+                ]),
+
+                     Column(
+
+                         children: <Widget>[
+
+                           Text(
+                             'Planes',
+                             textAlign: TextAlign.center,
+                             overflow: TextOverflow.ellipsis,
+                             style: TextStyle(fontWeight: FontWeight.bold, color: colorLetters, fontSize: 16),
+                           ),
+
+                         ]),
+                   ],
+
+                 ),
+                 Row(
+                   children: <Widget>[
+                     Icon(Icons.adjust, size: 10.0, color: Colors.transparent,),
+                     Text(
+                       'botones',
+                       textAlign: TextAlign.center,
+                       overflow: TextOverflow.ellipsis,
+                       style: TextStyle(fontWeight: FontWeight.bold, color: colorLetters, fontSize: 16),
+                     ),
+                   ],
+
+                 ),
+                 Row(
+                   children: <Widget>[
+                     Icon(Icons.adjust, size: 10.0, color: Colors.transparent,),
+                     Text(
+                       'paso 1',
+                       textAlign: TextAlign.center,
+                       overflow: TextOverflow.ellipsis,
+                       style: TextStyle(fontWeight: FontWeight.bold, color: colorLetters, fontSize: 16),
+                     ),
+
+                   ],
+
+                 ),
+                ],
+              ),
+            ),
 
           ],
         ),
+
+
       ),
     );
   }
