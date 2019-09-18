c17a9316e2889100fa2cbc2a9c2d4e6dcf7d5f81
cesarTix <cesar.sumano@tixmedia.com>
2019-09-18 12:26:57 -0500

    post method

--- cotizador_agente/lib/main.dart
+++ cotizador_agente/lib/main.dart
@@ -297,7 +297,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           SizedBox(
                             height: 10.0,
                           ),
-                          Text(" - ${snapshot.data.nombre}"),
+                          Text(" - ${snapshot.data.descripcion}"),
                         ]
                     ),
 
@@ -569,7 +569,44 @@ Future<Formulario> getQuote() async {
   }
 }
 
+class Post {
+  final String userId;
+  final int id;
+  final String title;
+  final String body;
 
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+
+  Map toMap() {
+    var map = new Map<String, dynamic>();
+    map["userId"] = userId;
+    map["title"] = title;
+    map["body"] = body;
+
+    return map;
+  }
+}
+
+
+Future<Post> createPost(String url, {Map body}) async {
+  return http.post(url, body: body).then((http.Response response) {
+    final int statusCode = response.statusCode;
+
+    if (statusCode < 200 || statusCode > 400 || json == null) {
+      throw new Exception("Error while fetching data");
+    }
+    return Post.fromJson(json.decode(response.body));
+  });
+}
 /*
 class SectionList {
 
