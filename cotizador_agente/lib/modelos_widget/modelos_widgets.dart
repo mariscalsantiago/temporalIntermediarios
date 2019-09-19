674cffaeb4ecede065e7a1185e907e10f93b4eff
cesarTix <cesar.sumano@tixmedia.com>
2019-09-19 10:32:38 -0500

    integration

--- cotizador_agente/lib/modelos_widget/modelos_widgets.dart
+++ cotizador_agente/lib/modelos_widget/modelos_widgets.dart
@@ -1,6 +1,7 @@
 
 import 'package:cotizador_agente/modelos/modelos.dart';
 import 'package:flutter/material.dart';
+import 'package:frideos_core/frideos_core.dart';
 
 
 
@@ -25,9 +26,6 @@ class ComboBox {
     this.valores});
 
 
-
-
-
   Widget getWidget(){
 
     DropdownButtonFormField(
@@ -37,8 +35,6 @@ class ComboBox {
           enabledBorder: UnderlineInputBorder(
               borderSide: BorderSide(color: Colors.orange))),
     );
-
-
 
   }
 
@@ -62,10 +58,125 @@ class ComboBox {
 
 
 
-
-
-
-
-
-
 }
+
+class TextBox{
+
+
+  final int ID;
+  final String etiqueta;
+  final bool obligatorio;
+  final String nombre_campo;
+  final String tipo_dato;
+  final String tipo_componente;
+  final bool visible;
+  final String regla;
+  final List <Campo>valores;
+  String _textLbChng;
+
+
+  TextBox({this.ID, this.etiqueta, this.obligatorio, this.nombre_campo,
+    this.tipo_dato, this.tipo_componente, this.visible, this.regla,
+    this.valores});
+
+
+
+  Widget prettyTextBox(){
+    getText();
+    TextFormField(
+        decoration: InputDecoration(
+        labelText: _textLbChng
+
+      ),
+    );
+
+  }
+
+  void changeHoldName(String textLabel){
+
+    _textLbChng = textLabel;
+  }
+
+  getText(){
+
+    for (Campo v in this.valores) {
+
+      changeHoldName(v.nombre_campo);
+    }
+  }
+}
+
+class Calendar{
+  final int ID;
+  final String etiqueta;
+  final bool obligatorio;
+  final String nombre_campo;
+  final String tipo_dato;
+  final String tipo_componente;
+  final bool visible;
+  final String regla;
+  final List <Campo>valores;
+  String _textLbChng;
+
+
+  Calendar({this.ID, this.etiqueta, this.obligatorio, this.nombre_campo,
+    this.tipo_dato, this.tipo_componente, this.visible, this.regla,
+    this.valores});
+}
+
+class Select{
+
+  final int ID;
+  final String etiqueta;
+  final bool obligatorio;
+  final String nombre_campo;
+  final String tipo_dato;
+  final String tipo_componente;
+  final bool visible;
+  final String regla;
+  final List <Campo>valores;
+  String _textLbChng;
+
+
+  Select({this.ID, this.etiqueta, this.obligatorio, this.nombre_campo,
+    this.tipo_dato, this.tipo_componente, this.visible, this.regla,
+    this.valores});
+}
+
+class CheckBox{
+
+  final int ID;
+  final String etiqueta;
+  final bool obligatorio;
+  final String nombre_campo;
+  final String tipo_dato;
+  final String tipo_componente;
+  final bool visible;
+  final String regla;
+  final List <Campo>valores;
+  String _textLbChng;
+  bool reNew = false;
+
+  CheckBox({this.ID, this.etiqueta, this.obligatorio, this.nombre_campo,
+    this.tipo_dato, this.tipo_componente, this.visible, this.regla,
+    this.valores});
+
+
+  Widget prettyCheckBox(){
+
+    /*
+    Checkbox(
+    value: reNew,
+    onChanged: (bool value) {
+    setState(() {
+    reNew = value;
+        });
+      },
+    );
+*/
+  }
+}
+
+
+
+
