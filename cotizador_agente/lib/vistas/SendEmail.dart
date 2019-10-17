d424fb31a1c0084211ccbc04767a511c5f820b5f
Yair <yair.zavaleta@gnp.com.mx>
2019-10-17 15:47:48 -0500

    Correcciones de Estilos en Enviar correo

--- cotizador_agente/lib/vistas/SendEmail.dart
+++ cotizador_agente/lib/vistas/SendEmail.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:cotizador_agente/main.dart';
 import 'package:cotizador_agente/utils/Utils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -22,87 +23,91 @@ class _SendEmailState extends State<SendEmail> {
       body: Column(
         children: <Widget>[
 
-        Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Row(
-            children: <Widget>[
-
-              Expanded(
-               flex: 1,
-                child: Text("Escoge la opción para envair la cotizacion"),
-
-              ),
-
-              Expanded(flex: 1,
+        Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(24),
+              child: Text("Escoge la opción para \nenviar la cotización",  overflow: TextOverflow.clip, style: TextStyle(fontSize: 20.0, color: Utilidades.color_titulo),
+                textAlign: TextAlign.left,),
+            ),
+            Spacer(),
+            Padding(
+              padding: EdgeInsets.only(right: 16, top: 32, bottom: 24),
               child: FloatingActionButton(
-                //onPressed: (),
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
                 heroTag: "btn1",
-                tooltip: "agrega beneficiario",
-                child: const Icon(Icons.clear),
+                tooltip: "Cerrar",
                 backgroundColor: Utilidades.color_primario,
-
-                ),
+                child: const Icon(Icons.close),
               ),
-
-
-            ],
-
-          ),
-
+            ),
+          ],
         ),
 
           Row(
             children: <Widget>[
              Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: new Text("Mail"),
+               padding: EdgeInsets.only(right: 24, left: 24),
+               child: new Text("Mail", style: TextStyle(fontWeight: FontWeight.w500, color: Utilidades.color_titulo),),
              ),
 
             ],
 
           ),
-           Row(
-            children: <Widget>[
-               Flexible(
-                 child: Padding(
-                   padding: const EdgeInsets.all(8.0),
-                   child: new TextFormField(
-                     decoration: new InputDecoration(
-                       labelText: "Enter Email",
-                       fillColor: Colors.white,
-                       border: new OutlineInputBorder(
-                         borderRadius: new BorderRadius.circular(0.0),
-                         borderSide: new BorderSide(
+           Padding(
+             padding: EdgeInsets.only(right: 24, left: 24, top: 4),
+             child: Row(
+
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(16.5),
+                  child: Icon(Icons.email, color: Utilidades.color_titulo,),
+                  decoration: BoxDecoration(
+                      border: Border(
+                        top:BorderSide(width: 1, color: Utilidades.color_titulo),
+                        bottom:BorderSide(width: 1, color: Utilidades.color_titulo),
+                        left:BorderSide(width: 1, color: Utilidades.color_titulo),
+                      )
+                  ),
+                ),
+                 Flexible(
+                   child: Padding(
+                     padding: EdgeInsets.only(right: 0, left: 0),
+                     child: new TextFormField(
+                       decoration: new InputDecoration(
+                         fillColor: Utilidades.color_titulo,
+                         border: new OutlineInputBorder(
+                           borderRadius: new BorderRadius.circular(0.0),
+                           borderSide: new BorderSide(
+                           ),
                          ),
                        ),
-
-                       icon: Icon(Icons.email),
-                       //fillColor: Colors.green
-                     ),
-                     validator: (val) {
-                       if(val.length==0) {
-                         return "Email cannot be empty";
-                       }else{
-                         return null;
-                       }
-                     },
-                     keyboardType: TextInputType.emailAddress,
-                     style: new TextStyle(
-                       fontFamily: "Poppins",
+                       validator: (val) {
+                         if(val.length==0) {
+                           return "Este no es un correo valido";
+                         }else{
+                           return null;
+                         }
+                       },
+                       keyboardType: TextInputType.emailAddress,
+                       style: new TextStyle(
+                         fontFamily: "Poppins",
+                       ),
                      ),
                    ),
-                 ),
-              ),
-
-            ],
-          ),
+                ),
+              ],
+            ),
+           ),
 
           Row(
             children: <Widget>[
 
               Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: new Text("Mensaje"),
+                padding: EdgeInsets.only(right: 24, left: 24, bottom: 4, top: 24),
+                child: new Text("Comentario", style: TextStyle(fontWeight: FontWeight.w500, color: Utilidades.color_titulo),),
               ),
             ],
 
@@ -112,7 +117,7 @@ class _SendEmailState extends State<SendEmail> {
             children: <Widget>[
               Flexible(
                 child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: EdgeInsets.only(right: 24, left: 24, bottom: 32),
                   child: new TextFormField(
                   keyboardType: TextInputType.multiline,
                     minLines: 3,
@@ -138,26 +143,32 @@ class _SendEmailState extends State<SendEmail> {
             ],
           ),
 
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              FlatButton(
-                color: Utilidades.color_primario,
-                textColor: Colors.white,
-                disabledColor: Utilidades.color_primario,
-                disabledTextColor: Colors.white,
-                padding: EdgeInsets.all(8.0),
-                splashColor: Utilidades.color_titulo,
-                //onPressed: (),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "ENVIAR",
-                    style: TextStyle(fontSize: 16.0, letterSpacing: 1),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(right: 24, left: 24, bottom: 32),
+                child: Container(
+                  width: double.infinity,
+                  child: FlatButton(
+                    color: Utilidades.color_primario,
+                    textColor: Colors.white,
+                    disabledColor: Utilidades.color_primario,
+                    disabledTextColor: Colors.white,
+                    padding: EdgeInsets.all(8.0),
+                    splashColor: Utilidades.color_titulo,
+                    //onPressed: (),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "ENVIAR",
+                        style: TextStyle(fontSize: 16.0, letterSpacing: 1),
+                      ),
+                    ),
                   ),
                 ),
-              )
-            ],
+              ),
+            ),
           )
         ],
       ),
