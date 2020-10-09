92a8b65c864a3c3161034ec307bfdf5c8f804fdb
Fernanda Rivera <bianca.rivera@gnp.com.mx>
2020-10-09 11:38:48 -0500

    Conexión

---
+++ cotizador_agente/lib/RequestHandler/MyRequest.dart
@@ -0,0 +1,11 @@
+import 'package:cotizador_agente/RequestHandler/RequestHandler.dart';
+
+class MyRequest {
+  String baseUrl;
+  String path;
+  Method method;
+  String body;
+  Map<String, String> headers;
+
+  MyRequest({this.baseUrl, this.path, this.method, this.body, this.headers});
+}
