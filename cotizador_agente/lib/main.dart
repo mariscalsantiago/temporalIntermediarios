941f8fe61877fe08994b89efd5c153ebe53b4820
DaliaOrepezaBello <=>
2021-06-18 18:26:45 -0500

    Cambio urls PRO

--- cotizador_agente/lib/main.dart
+++ cotizador_agente/lib/main.dart
@@ -28,10 +28,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
 
-    var configuredApp = new AppConfig(
+  var configuredApp = new AppConfig(
     ambient: Ambient.prod,
     serviceLogin: 'https://cuentas.gnp.com.mx/auth/login',
-    apikeyAppAgentes: 'l7xxed71b90a2ed941668463e3a01513d582',
+    apikeyAppAgentes: 'COLOCAR APIKEY GNP_Flutter_AppInter',
     service_perfilador: "https://api.service.oscp.gnp.com.mx/cmn-intermediario/consulta-perfil-app",
     proyectId: 'gnp-accidentespersonales-pro',
     // urlNotifierService:'https://api.service.gnp.com.mx',
@@ -42,22 +42,22 @@ void main() async {
     urlSendAnalytics: 'https://www.google-analytics.com/',
     idContenedorAnalytics: 'UA-29272127-16',
 
-    serviceBCA: 'https://bca-ws-qa.gnp.com.mx',
-    apikeyBCA: 'd41d8cd98f00b204e9800998ecf8427e',
+    serviceBCA: 'https://bca-ws.gnp.com.mx',
+    apikeyBCA: '9a780a70-c5fc-4bee-86cf-5650cce16516',
 
     //Preguntas Secretas
-    apiKey: 'l7xx2fea6d294a6641438cde5681449e16dc',
-    consultaPreguntasSecretas: 'https://api-qa.oscp.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas/',
-    actualizarEstablecerPreguntasSecretas: 'https://api-qa.oscp.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas',
-    cambioContrasenaPerfil: 'https://api-qa.oscp.gnp.com.mx/aprAprovisionamientoProvee/intermediario/password/',
-    reestablecerContrasena: 'https://api-qa.oscp.gnp.com.mx/aprAprovisionamiento/admonUsuarios',
-    consultaUsuarioPorCorreo: 'https://api-qa.oscp.gnp.com.mx/aprAprovisionamiento/admonUsuarios',
+    apiKey: 'COLOCAR APIKEY GNP_Flutter_AppInter',
+    consultaPreguntasSecretas: 'https://api.service.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas/',
+    actualizarEstablecerPreguntasSecretas: 'https://api.service.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas',
+    cambioContrasenaPerfil: 'https://api.service.gnp.com.mx/aprAprovisionamientoProvee/intermediario/password/',
+    reestablecerContrasena: 'https://api.service.gnp.com.mx/aprAprovisionamiento/admonUsuarios',
+    consultaUsuarioPorCorreo: 'https://api.service.gnp.com.mx/aprAprovisionamiento/admonUsuarios',
     orquestadorOTPSinSesion:'https://app-inter.gnp.com.mx/intermediario/enviarOtp/sinSesion',
-    validaOTP: 'https://api-qa.oscp.gnp.com.mx/apr/otpLocalService/validateOtp/',
-    consultarMedioContactosAgentes :'https://api-qa.oscp.gnp.com.mx/crm-personas/consulta-medios-contacto-agt-id?idAgente=',
-    altaMediosContactoAgentes:'https://api-qa.oscp.gnp.com.mx/crm-personas/alta-medios-contacto-evo',
-    orquestadorOtpJwt:'https://orquestador-otp-dot-gnp-accidentespersonales-qa.appspot.com/intermediario/enviarOtp/me',
-    consultaPersonaIdParticipante:"https://api-qa.oscp.gnp.com.mx/CRM/ConsultaPersonaIdParticipante",
+    validaOTP: 'https://api.service.gnp.com.mx/apr/otpLocalService/validateOtp/',
+    consultarMedioContactosAgentes :'https://api.service.gnp.com.mx/crm-personas/consulta-medios-contacto-agt-id?idAgente=',
+    altaMediosContactoAgentes:'https://api.service.gnp.com.mx/crm-personas/alta-medios-contacto-evo',
+    orquestadorOtpJwt:'https://app-inter.gnp.com.mx/intermediario/enviarOtp/me',
+    consultaPersonaIdParticipante:"https://api.service.gnp.com.mx/CRM/ConsultaPersonaIdParticipante",
 
     child: new MyApp(),
 
