1107e04f452fe2281e42f4c4cb196f77683318b4
DaliaOrepezaBello <=>
2021-09-02 17:03:26 -0500

    Cambio versioon para PRO

--- cotizador_agente/lib/main.dart
+++ cotizador_agente/lib/main.dart
@@ -33,37 +33,38 @@ void main() async {
 
 
   var configuredApp = new AppConfig(
-    ambient: Ambient.uat,
-    serviceLogin: 'https://cuentas-uat.gnp.com.mx/auth/login',
-    apikeyAppAgentes: 'l7xx526ec2d1bd9140a39ad15f72e1964bca',
-    service_perfilador: "https://api-uat.oscp.gnp.com.mx/Consulta-Agentes/consulta-perfil-app-int",
-    proyectId: "gnp-accidentespersonales-uat",
-    // urlNotifierService:'https://api-uat.oscpuat.gnp.com.mx',
-    //COTIZADOR UNICO
-    urlNegociosOperables: 'https://us-central1-gnp-auttarifasgmm-uat.cloudfunctions.net/',
-    urlBase: 'https://gmm-cotizadores-uat.gnp.com.mx/',
-    urlSendAnalytics: 'https://www.google-analytics.com/',
-    idContenedorAnalytics: 'UA-146126625-1',
+    ambient: Ambient.prod,
+    serviceLogin: 'https://cuentas.gnp.com.mx/auth/login',
+    apikeyAppAgentes: 'COLOCAR APIKEY GNP_Flutter_AppInter',
+    service_perfilador: "https://api.service.gnp.com.mx/Consulta-Agentes/consulta-perfil-app-int",
+    proyectId: 'gnp-accidentespersonales-pro',
+    // urlNotifierService:'https://api.service.gnp.com.mx',
 
-    serviceBCA: 'https://bca-ws-uat.gnp.com.mx',
-    apikeyBCA: 'd41d8cd98f00b204e9800998ecf8427e',
+    //COTIZADOR UNICO
+    urlNegociosOperables: 'https://us-central1-gnp-auttarifasgmm-pro.cloudfunctions.net/',
+    urlBase: 'https://gmm-cotizadores.gnp.com.mx/',
+    urlSendAnalytics: 'https://www.google-analytics.com/',
+    idContenedorAnalytics: 'UA-29272127-16',
+
+    serviceBCA: 'https://bca-ws.gnp.com.mx',
+    apikeyBCA: '9a780a70-c5fc-4bee-86cf-5650cce16516',
 
     //Preguntas Secretas
-    apiKey: 'l7xx14f3730ee0e849f1a0108359030ee9fb',
-    consultaPreguntasSecretas: 'https://api-uat.oscpuat.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas/',
-    actualizarEstablecerPreguntasSecretas: 'https://api-uat.oscpuat.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas',
-    cambioContrasenaPerfil: 'https://api-uat.oscpuat.gnp.com.mx/aprAprovisionamientoProvee/intermediario/password/',
-    reestablecerContrasena: 'https://api-uat.oscpuat.gnp.com.mx/aprAprovisionamiento/admonUsuarios',
-    consultaUsuarioPorCorreo: 'https://api-uat.oscpuat.gnp.com.mx/aprAprovisionamiento/admonUsuarios',
-    orquestadorOTPSinSesion:'https://app-inter-uat.gnp.com.mx/intermediario/enviarOtp/sinSesion',
-    validaOTP: 'https://api-uat.oscpuat.gnp.com.mx/apr/otpLocalService/validateOtp/',
-    consultarMedioContactosAgentes :'https://api-uat.oscpuat.gnp.com.mx/crm-personas/consulta-medios-contacto-agt-id?idAgente=',
-    altaMediosContactoAgentes:'https://api-uat.oscpuat.gnp.com.mx/crm-personas/alta-medios-contacto-evo',
-    orquestadorOtpJwt:'https://gnp-accidentespersonales-uat.uc.r.appspot.com/intermediario/enviarOtp/me',
-    consultaPersonaIdParticipante:"https://api-uat.oscpuat.gnp.com.mx/CRM/ConsultaPersonaIdParticipante",
-    cotizadorAutos: "https://gnp-appcontratacionautos-uat.uc.r.appspot.com/",
-    servicioNuevoConsultaPorCorreo:"https://api-uat.oscpuat.gnp.com.mx/aprAprovisionamientoProvee/intermediario/app/consulta-usuario-correo?email=",
-
+    apiKey: 'COLOCAR APIKEY GNP_Flutter_AppInter',
+    consultaPreguntasSecretas: 'https://api.service.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas/',
+    actualizarEstablecerPreguntasSecretas: 'https://api.service.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas',
+    cambioContrasenaPerfil: 'https://api.service.gnp.com.mx/aprAprovisionamientoProvee/intermediario/password/',
+    reestablecerContrasena: 'https://api.service.gnp.com.mx/aprAprovisionamiento/admonUsuarios',
+    consultaUsuarioPorCorreo: 'https://api.service.gnp.com.mx/aprAprovisionamiento/admonUsuarios',
+    orquestadorOTPSinSesion:'https://app-inter.gnp.com.mx/intermediario/enviarOtp/sinSesion',
+    validaOTP: 'https://api.service.gnp.com.mx/apr/otpLocalService/validateOtp/',
+    consultarMedioContactosAgentes :'https://api.service.gnp.com.mx/crm-personas/consulta-medios-contacto-agt-id?idAgente=',
+    altaMediosContactoAgentes:'https://api.service.gnp.com.mx/crm-personas/alta-medios-contacto-evo',
+    orquestadorOtpJwt:'https://app-inter.gnp.com.mx/intermediario/enviarOtp/me',
+    consultaPersonaIdParticipante:"https://api.service.gnp.com.mx/CRM/ConsultaPersonaIdParticipante",
+    cotizadorAutos: "https://gnp-appcontratacionautos-pro.uc.r.appspot.com/",
+    servicioNuevoConsultaPorCorreo:"https://api.service.gnp.com.mx/aprAprovisionamientoProvee/intermediario/app/consulta-usuario-correo?email=",
+//https://api.service.gnp.com.mx/aprAprovisionamientoProvee/intermediario/app/consulta-usuario-correo
     child: new MyApp(),
 
   );
