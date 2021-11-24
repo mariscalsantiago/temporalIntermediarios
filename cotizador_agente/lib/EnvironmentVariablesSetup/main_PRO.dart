581bf3df3ebcd832917a7ba4a12aaaa3b1c44709
DaliaOrepezaBello <=>
2021-11-24 12:56:55 -0600

    2.8.1

--- cotizador_agente/lib/EnvironmentVariablesSetup/main_PRO.dart
+++ cotizador_agente/lib/EnvironmentVariablesSetup/main_PRO.dart
@@ -13,13 +13,13 @@ void main() {
   var configuredApp = new AppConfig(
     ambient: Ambient.prod,
     serviceLogin: 'https://cuentas.gnp.com.mx/auth/login',
-    apikeyAppAgentes: 'l7xx9d2ed3d704454924ba7bbc4f80cb56ea',
-    service_perfilador: "https://api.service.gnp.com.mx/Consulta-Agentes/consulta-perfil-app-int",
+    apikeyAppAgentes: 'COLOCAR APIKEY GNP_Flutter_AppInter',
+    service_perfilador: "https://app-inter.gnp.com.mx/Consulta-Agentes/consulta-perfil-app-int",
     proyectId: 'gnp-accidentespersonales-pro',
     // urlNotifierService:'https://api.service.gnp.com.mx',
 
     //COTIZADOR UNICO
-    urlNegociosOperables: 'https://us-central1-gnp-auttarifasgmm-pro.cloudfunctions.net/',
+    urlNegociosOperables: 'https://app-inter.gnp.com.mx/COT_CF_ConsultaNegocioCanal',
     urlBase: 'https://gmm-cotizadores.gnp.com.mx/',
     urlSendAnalytics: 'https://www.google-analytics.com/',
     idContenedorAnalytics: 'UA-29272127-16',
@@ -28,23 +28,25 @@ void main() {
     apikeyBCA: '9a780a70-c5fc-4bee-86cf-5650cce16516',
 
     //Preguntas Secretas
-    apiKey: 'l7xx9d2ed3d704454924ba7bbc4f80cb56ea',
-    consultaPreguntasSecretas: 'https://api.service.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas/',
-    actualizarEstablecerPreguntasSecretas: 'https://api.service.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas',
-    cambioContrasenaPerfil: 'https://api.service.gnp.com.mx/aprAprovisionamientoProvee/intermediario/password/',
-    reestablecerContrasena: 'https://api.service.gnp.com.mx/aprAprovisionamiento/admonUsuarios',
-    consultaUsuarioPorCorreo: 'https://api.service.gnp.com.mx/aprAprovisionamiento/admonUsuarios',
+    apiKey: 'COLOCAR APIKEY GNP_Flutter_AppInter',
+    consultaPreguntasSecretas: 'https://app-inter.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas/',
+    actualizarEstablecerPreguntasSecretas: 'https://app-inter.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas',
+    cambioContrasenaPerfil: 'https://app-inter.gnp.com.mx/aprAprovisionamientoProvee/intermediario/password/',
+    reestablecerContrasena: 'https://app-inter.gnp.com.mx/aprAprovisionamiento/admonUsuarios',
+    consultaUsuarioPorCorreo: 'https://app-inter.gnp.com.mx/aprAprovisionamientoProvee/intermediario/app/consulta-usuario-correo?email=',
     orquestadorOTPSinSesion:'https://app-inter.gnp.com.mx/intermediario/enviarOtp/sinSesion',
-    validaOTP: 'https://api.service.gnp.com.mx/apr/otpLocalService/validateOtp/',
-    consultarMedioContactosAgentes :'https://api.service.gnp.com.mx/crm-personas/consulta-medios-contacto-agt-id?idAgente=',
-    altaMediosContactoAgentes:'https://api.service.gnp.com.mx/crm-personas/alta-medios-contacto-evo',
-    orquestadorOtpJwt:'https://app-inter.gnp.com.mx/intermediario/enviarOtp/me',
-    consultaPersonaIdParticipante:"https://api.service.gnp.com.mx/CRM/ConsultaPersonaIdParticipante",
-    cotizadorAutos: "https://gnp-appcontratacionautos-pro.uc.r.appspot.com/",
-    servicioNuevoConsultaPorCorreo:"https://api.service.gnp.com.mx/aprAprovisionamientoProvee/intermediario/app/consulta-usuario-correo?email=",
-//https://api.service.gnp.com.mx/aprAprovisionamientoProvee/intermediario/app/consulta-usuario-correo
+    validaOTP: 'https://app-inter.gnp.com.mx/apr/otpLocalService/validateOtp/',
+    consultarMedioContactosAgentes :'https://app-inter.gnp.com.mx/crm-personas/consulta-medios-contacto-agt-id?idAgente=',
+    altaMediosContactoAgentes:'https://app-inter.gnp.com.mx/crm-personas/alta-medios-contacto-evo',
+    orquestadorOtpJwt:'https://gnp-accidentespersonales-uat.uc.r.appspot.com/intermediario/enviarOtp/me',
+    consultaPersonaIdParticipante:"https://app-inter.gnp.com.mx/CRM/ConsultaPersonaIdParticipante",
+    cotizadorAutos: "https://gnp-appcontratacionautos-uat.uc.r.appspot.com/",
+    servicioNuevoConsultaPorCorreo:"https://app-inter.gnp.com.mx/aprAprovisionamientoProvee/intermediario/app/consulta-usuario-correo?email=",
+    servicioNuevoDatosPerfil:"https://app-inter.gnp.com.mx/app/datos-perfil/",
+
     child: new MyApp(),
 
   );
+
   runApp(configuredApp);
 }
--- cotizador_agente/lib/main.dart
+++ cotizador_agente/lib/main.dart
@@ -34,36 +34,38 @@ void main() async {
 
 
   var configuredApp = new AppConfig(
-    ambient: Ambient.uat,
-    serviceLogin: 'https://cuentas-uat.gnp.com.mx/auth/login',
-    apikeyAppAgentes: 'l7xx526ec2d1bd9140a39ad15f72e1964bca',
-    service_perfilador: "https://app-inter-uat.gnp.com.mx/Consulta-Agentes/consulta-perfil-app-int",
-    proyectId: "gnp-accidentespersonales-uat",
-    // urlNotifierService:'https://api-uat.oscpuat.gnp.com.mx',
-    //COTIZADOR UNICO
-    urlNegociosOperables: 'https://app-inter-uat.gnp.com.mx/COT_CF_ConsultaNegocioCanal',
-    urlBase: 'https://gmm-cotizadores-uat.gnp.com.mx/',
-    urlSendAnalytics: 'https://www.google-analytics.com/',
-    idContenedorAnalytics: 'UA-146126625-1',
+    ambient: Ambient.prod,
+    serviceLogin: 'https://cuentas.gnp.com.mx/auth/login',
+    apikeyAppAgentes: 'COLOCAR APIKEY GNP_Flutter_AppInter',
+    service_perfilador: "https://app-inter.gnp.com.mx/Consulta-Agentes/consulta-perfil-app-int",
+    proyectId: 'gnp-accidentespersonales-pro',
+    // urlNotifierService:'https://api.service.gnp.com.mx',
 
-    serviceBCA: 'https://bca-ws-uat.gnp.com.mx',
-    apikeyBCA: 'd41d8cd98f00b204e9800998ecf8427e',
+    //COTIZADOR UNICO
+    urlNegociosOperables: 'https://app-inter.gnp.com.mx/COT_CF_ConsultaNegocioCanal',
+    urlBase: 'https://gmm-cotizadores.gnp.com.mx/',
+    urlSendAnalytics: 'https://www.google-analytics.com/',
+    idContenedorAnalytics: 'UA-29272127-16',
+
+    serviceBCA: 'https://bca-ws.gnp.com.mx',
+    apikeyBCA: '9a780a70-c5fc-4bee-86cf-5650cce16516',
 
     //Preguntas Secretas
-    apiKey: 'l7xx14f3730ee0e849f1a0108359030ee9fb',
-    consultaPreguntasSecretas: 'https://app-inter-uat.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas/',
-    actualizarEstablecerPreguntasSecretas: 'https://app-inter-uat.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas',
-    cambioContrasenaPerfil: 'https://app-inter-uat.gnp.com.mx/aprAprovisionamientoProvee/intermediario/password/',
-    reestablecerContrasena: 'https://app-inter-uat.gnp.com.mx/aprAprovisionamiento/admonUsuarios',
-    consultaUsuarioPorCorreo: 'https://app-inter-uat.gnp.com.mx/aprAprovisionamientoProvee/intermediario/app/consulta-usuario-correo?email=',
-    orquestadorOTPSinSesion:'https://app-inter-uat.gnp.com.mx/intermediario/enviarOtp/sinSesion',
-    validaOTP: 'https://app-inter-uat.gnp.com.mx/apr/otpLocalService/validateOtp/',
-    consultarMedioContactosAgentes :'https://api-uat.oscpuat.gnp.com.mx/crm-personas/consulta-medios-contacto-agt-id?idAgente=',
-    altaMediosContactoAgentes:'https://app-inter-uat.gnp.com.mx/crm-personas/alta-medios-contacto-evo',
+    apiKey: 'COLOCAR APIKEY GNP_Flutter_AppInter',
+    consultaPreguntasSecretas: 'https://app-inter.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas/',
+    actualizarEstablecerPreguntasSecretas: 'https://app-inter.gnp.com.mx/aprAprovisionamientoProvee/intermediario/preguntas',
+    cambioContrasenaPerfil: 'https://app-inter.gnp.com.mx/aprAprovisionamientoProvee/intermediario/password/',
+    reestablecerContrasena: 'https://app-inter.gnp.com.mx/aprAprovisionamiento/admonUsuarios',
+    consultaUsuarioPorCorreo: 'https://app-inter.gnp.com.mx/aprAprovisionamientoProvee/intermediario/app/consulta-usuario-correo?email=',
+    orquestadorOTPSinSesion:'https://app-inter.gnp.com.mx/intermediario/enviarOtp/sinSesion',
+    validaOTP: 'https://app-inter.gnp.com.mx/apr/otpLocalService/validateOtp/',
+    consultarMedioContactosAgentes :'https://app-inter.gnp.com.mx/crm-personas/consulta-medios-contacto-agt-id?idAgente=',
+    altaMediosContactoAgentes:'https://app-inter.gnp.com.mx/crm-personas/alta-medios-contacto-evo',
     orquestadorOtpJwt:'https://gnp-accidentespersonales-uat.uc.r.appspot.com/intermediario/enviarOtp/me',
-    consultaPersonaIdParticipante:"https://app-inter-uat.gnp.com.mx/CRM/ConsultaPersonaIdParticipante",
+    consultaPersonaIdParticipante:"https://app-inter.gnp.com.mx/CRM/ConsultaPersonaIdParticipante",
     cotizadorAutos: "https://gnp-appcontratacionautos-uat.uc.r.appspot.com/",
-    servicioNuevoConsultaPorCorreo:"https://app-inter-uat.gnp.com.mx/aprAprovisionamientoProvee/intermediario/app/consulta-usuario-correo?email=",
+    servicioNuevoConsultaPorCorreo:"https://app-inter.gnp.com.mx/aprAprovisionamientoProvee/intermediario/app/consulta-usuario-correo?email=",
+    servicioNuevoDatosPerfil:"https://app-inter.gnp.com.mx/app/datos-perfil/",
 
     child: new MyApp(),
 
