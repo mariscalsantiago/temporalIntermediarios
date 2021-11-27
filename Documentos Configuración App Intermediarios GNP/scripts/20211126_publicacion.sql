7181a8ea76078ea3e9a4d9f6f706095d6388d70b
Moises Cristopher Gómez Santos <moises.gomez@gnp.com.mx>
2021-11-26 18:59:28 -0600

    ActualizarDocumentos Configuración App Intermediarios GNP/scripts/20211126_publicacion.sql

---
+++ Documentos Configuración App Intermediarios GNP/scripts/20211126_publicacion.sql
@@ -0,0 +1,5 @@
+
+insert into dbappstore.aps_version_app(id_app,version,id_os,fecha_publicacion,requerida) values (24,'1_0_3',2, '2021-11-27 06:00:00', true );
+
+
+insert into dbappstore.aps_version_app(id_app,version,id_os,fecha_publicacion,requerida) values (24,'1_0_3',1, '2021-11-27 06:00:00', true );
